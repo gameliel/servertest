1a81a77a508db5552a67055dafebd467b6d32ba7
gameliel <eugeneibiso@gmail.com>
2021-05-27 12:36:46 +0000

    rumples loading..

--- apps/coupon/admin.py
+++ apps/coupon/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from .models import Coupon
 
-admin.site.register(Coupon)
+@admin.register(Coupon)
+class CouponAdmin(admin.ModelAdmin):
+    list_display = ('code', 'value', 'num_available', 'num_used')
+    search_fields = ('code', 'value')
--- apps/store/admin.py
+++ apps/store/admin.py
@@ -2,7 +2,20 @@ from django.contrib import admin
 
 from .models import Category, Product, ProductImage, ProductReview
 
-admin.site.register(Category)
-admin.site.register(Product)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title', )}
+    list_display = ('title', 'slug')
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title', )}
+    list_display = ('title', 'category', 'price', 'parent', 'date_added', 'last_visit')
+    search_fields = ['title', 'slug', 'description']
+
 admin.site.register(ProductImage)
-admin.site.register(ProductReview)
+
+@admin.register(ProductReview)
+class ProductReviewAdmin(admin.ModelAdmin):
+    list_display = ('product', 'user', 'stars', 'date_added')
